4132beb98108672fc9bfe2f924ef20a5654906b6
TiltdBastion <vincent.aymonin@gmail.com>
2025-07-06 16:48:01 +0200

    Fix login redirect url

--- CarDash/CarDash/settings.py
+++ CarDash/CarDash/settings.py
@@ -128,5 +128,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'vehicle_selection'
 LOGOUT_REDIRECT_URL = 'login'
