0ef5302b66c66b25ef1c2d93f83f98b7940c152b
TiltdBastion <vincent.aymonin@gmail.com>
2025-08-31 21:24:53 +0200

    Link categories to user

--- CarDash/main/models/category.py
+++ CarDash/main/models/category.py
@@ -1,13 +1,15 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+
 class CategoryQuerySet(models.query.QuerySet):
     def for_user(self, user):
         return self.filter(user=user)
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    user = get_user_model()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     objects = CategoryQuerySet.as_manager()
 
