40f39bcdb38142170efb0ace48cd108b053c1d60
TiltdBastion <vincent.aymonin@gmail.com>
2025-08-31 21:34:37 +0200

    Remove config page and light/dark theme switch

--- CarDash/main/urls.py
+++ CarDash/main/urls.py
@@ -4,5 +4,4 @@ from .views import dashboard_view, config_view, vehicle_selection_view
 urlpatterns = [
     path('vehicle_selection/', vehicle_selection_view, name='vehicle_selection'),
     path('dashboard/<int:car_id>/', dashboard_view, name='dashboard'),
-    path('configuration/<int:car_id>/', config_view, name='config')
 ]
