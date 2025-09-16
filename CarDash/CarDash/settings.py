f79ebc0918d7d05d97eb5e5e06f1db59722ff09b
TiltdBastion <vincent.aymonin@gmail.com>
2025-09-16 22:59:29 +0200

    Update Postgres version + fix env file path

--- CarDash/CarDash/settings.py
+++ CarDash/CarDash/settings.py
@@ -15,7 +15,7 @@ import os
 import sys
 from dotenv import load_dotenv
 
-load_dotenv('.env')
+load_dotenv('../.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
